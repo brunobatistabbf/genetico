f7fee63f52377ed81594bf67cc4bf2e96b466085
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2023-03-16 03:24:07 -0300

    update

--- main.py
+++ main.py
@@ -4,36 +4,37 @@ import random
 alfabeto = "abcdefghijklmnopqrstuvwxyz "
 alvo = "ciencia da computacao"
 
-# Define o tamanho da população e número máximo de gerações
+# Tamanho da população e número máximo de gerações
 tam_pop = 200
 max_geracoes = 2000
 
 
-# Define a função de fitness
 def fitness(individuo):
+    """
+    Função de avaliação. Retorna a quantidade de caracteres corretos no indivíduo.
+    """
     return sum(1 for a, b in zip(individuo, alvo) if a == b)
 
 
-# Gera a população inicial
+# Cria uma população inicial de indivíduos aleatórios
 populacao = [''.join(random.choice(alfabeto) for _ in range(len(alvo))) for _ in range(tam_pop)]
 
-# Evolui a população por um número máximo de gerações
-for geracao in range(max_geracoes):
-    # Avalia o fitness de cada indivíduo
-    avaliacoes = [(individuo, fitness(individuo)) for individuo in populacao]
 
-    # Ordena a população pelo fitness, do maior para o menor
+for geracao in range(max_geracoes):
+
+    # Avalia cada indivíduo na população e ordena por fitness (mais alto primeiro)
+    avaliacoes = [(individuo, fitness(individuo)) for individuo in populacao]
     avaliacoes.sort(key=lambda x: x[1], reverse=True)
 
-    # Verifica se a frase alvo foi encontrada
+    # Se um indivíduo atinge a solução, termina a execução
     if avaliacoes[0][0] == alvo:
-        print(f"Encontrado na geração {geracao}")
+        print(f"Melhor solução encontrada na geração {geracao}: {avaliacoes[0][0]}")
         break
 
-    # Seleciona os indivíduos mais aptos para reprodução
+    # Seleciona os pais para a próxima geração (50% melhores indivíduos)
     pais = [avaliacoes[i][0] for i in range(tam_pop // 2)]
 
-    # Realiza o crossover para gerar novos indivíduos
+    # Cria novos indivíduos (filhos) através do cruzamento de dois pais aleatórios
     filhos = []
     for i in range(tam_pop - len(pais)):
         pai = random.choice(pais)
@@ -41,19 +42,17 @@ for geracao in range(max_geracoes):
         ponto_corte = random.randint(1, len(alvo) - 1)
         filho = pai[:ponto_corte] + mae[ponto_corte:]
         filhos.append(filho)
-        print(filho)
 
-
-    # Mutação em uma taxa de 5%
+    # Realiza uma mutação aleatória (troca de um caractere) em alguns filhos (1% de chance)
     for i in range(len(filhos)):
         if random.random() < 0.01:
             filho = list(filhos[i])
             filho[random.randint(0, len(alvo) - 1)] = random.choice(alfabeto)
             filhos[i] = ''.join(filho)
 
-
-    # Nova população é a união dos pais com os filhos
+    # Nova população é composta pelos pais selecionados e os novos filhos
     populacao = pais + filhos
+    print(populacao)
 
 
 
